ffcbc6285575e35798e844929c5998b383ee91df
Stefano Silvestrini <silver.stefano@gmail.com>
2019-06-24 11:07:00 +0200

    app.get inserted, new file html

--- index.js
+++ index.js
@@ -4,7 +4,10 @@ const path= require(‘path’)
 
 app.use(express.static(path.join(__dirname, 'client')))
 
-
+app.get('/', (req, res) => {
+	res.sendFile(path.join(__dirname, '/client/airbnb.html'))
+	console.log('__dirname', __dirname)
+})
 
 
 
