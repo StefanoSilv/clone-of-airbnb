b1aa84bf4b1cc89537700eacaad19ed297c53600
Stefano Silvestrini <silver.stefano@gmail.com>
2019-06-24 10:59:14 +0200

    app.listen added

--- index.js
+++ index.js
@@ -1,2 +1,19 @@
 const express = require(‘express') //Require express
 const app = express() //The function app
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+app.listen(3010, () => {
+	console.log("The server airbnb is running on port 3010");
+})
