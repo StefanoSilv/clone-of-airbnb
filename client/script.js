37f2360e3d380fc1b8e2065ea8153ef1f7a7e8be
Stefano Silvestrini <silver.stefano@gmail.com>
2019-06-26 05:26:54 +0200

    filter on country working

--- client/script.js
+++ client/script.js
@@ -1,39 +1,51 @@
 window.onload= () =>{
-	//Houses in the DOM
-	axios.get('/api/houses').then( (res) => {
-		let results = res.data
-		let pointer= document.getElementById('houses-grid')
-
-		results.forEach( (i) => {
-			pointer.insertAdjacentHTML('afterBegin', `<div class="houses">
-				<div class="houses-imagine">
-				</div>
-				<div class="house-info">
-					<h2>${i.name}</h2>
-					<h3>${i.price}</h3>
-					<h4>${i.rating}</h4>
-				</div>
-			</div>`)
-		})
-	}).catch ((err) => {
-		console.log(err);
-	})
-
+	// Countries in the DOM
 	axios.get('/api/countries').then( (res)=>{
-		let cities= res.data
-		let city_pointer= document.getElementById('city-banner')
+		let country= res.data
+		let country_pointer= document.getElementById('country-banner')
 
-		cities.forEach( (c)=> {
-			city_pointer.insertAdjacentHTML('afterBegin',`<div class="city">
-				<div class="city-imagine">
+		country.forEach( (c)=> {
+			country_pointer.insertAdjacentHTML('afterBegin',`
+			<a class="country_minibanner">
+				<div class="country-imagine">
 				</div>
-				<div class="city-info">
-				<h3>${c.name}</h3>
+				<div class="country-info">
+				<h3 class="random_name" id=${c.id}>${c.name}</h3>
 				</div>
-			</div>
+			</a>
 			`)
 		})
 	}).catch((err) => {
 		console.log(err);
+	})
+
+
+	//Houses in the DOM
+	document.addEventListener( 'click', (e) =>{
+		let target= e.target.id
+		console.log(e.target);
+		if  (e.target.classList.contains('random_name')){
+			axios.get(`/api/houses?country=${e.target.id}`).then( (res) => {
+				let results = res.data
+				console.log(results);
+				console.log('hello');
+				let pointer= document.getElementById('houses-grid')
+				pointer.innerHTML = ''
+
+				results.forEach( (i) => {
+					pointer.insertAdjacentHTML('afterBegin', `<div class="houses">
+						<div class="houses-imagine">
+						</div>
+						<div class="house-info">
+							<h2>${i.name}</h2>
+							<h3>${i.price}</h3>
+							<h4>${i.rating}</h4>
+						</div>
+					</div>`)
+				})
+			}).catch ((err) => {
+				console.log(err);
+			})
+		}
 	})
 }
